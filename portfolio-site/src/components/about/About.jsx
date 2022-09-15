8b5eb3720573fd5ccb9466164263d0af77bf385f
tyler-deodat <tylerkdeodat@gmail.com>
2022-09-15 18:52:47 -0400

    about and contact section complete

---
+++ portfolio-site/src/components/about/About.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+// import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1 Year Working</small>
+            </article>
+
+            {/* <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>4 Worldwide</small>
+            </article> */}
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>5 Completed</small>
+            </article>
+          </div>
+
+          <p>
+            I am a Full Stack Developer based in Tampa, FL. After several years
+            in the military I am transitioning into web development with a
+            specialty in building and designing web applications. Outside of
+            work I am often found hiking, kayaking, or working on vintage cars.
+            Don't hesitate to reach out and leave me a message!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
